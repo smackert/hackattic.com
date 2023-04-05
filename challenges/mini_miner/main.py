41804a7f2ed442d146d04872f6160def802686d7
Sean Mackert <smackert@users.noreply.github.com>
2023-04-05 16:03:01 +0400

    mini_miner: add: solution submission
    Added solution submission. Refactored main.
    And maybe I switched to Pydandic
     and didn't commit it.Shhh.

--- challenges/mini_miner/main.py
+++ challenges/mini_miner/main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import math
 from dotenv import load_dotenv
 import hashlib
 from json_challenge import Challenge, Block
@@ -8,11 +9,12 @@ from json_challenge import Challenge, Block
 load_dotenv()
 base_url = os.getenv("CHALLENGES_BASE_URL")
 problem_endpoint = os.getenv("PROBLEM_ENDPOINT")
+solution_endpoint = os.getenv("SOLUTION_ENDPOINT")
 access_token = os.getenv("HACKATTIC_ACCESS_TOKEN")
 
 challenge_name = "mini_miner"
 api_challenge_endpoint = base_url + challenge_name + problem_endpoint + access_token
-
+apit_solution_endpoint = base_url + challenge_name + solution_endpoint + access_token
 
 def get_challenge_data():
     r = requests.get(api_challenge_endpoint)
@@ -24,8 +26,33 @@ def get_challenge_data():
         return None
 
 
-challenge_data = get_challenge_data()
-challenge = Challenge.from_json(challenge_data)
+def brute_force(challenge):
+    hash = ""
+    nonce = 0
+    zeros_needed = math.ceil(challenge.difficulty / 2)
+    print(f"Searching for hash with {zeros_needed} zeros")
+    while hash[:zeros_needed] != (zeros_needed) * "0":
+        challenge.block.nonce = nonce
+        sha = hashlib.sha256()
+        sha.update(json.dumps(dict(challenge.block), separators=(",", ":")).encode())
+        hash = sha.hexdigest()
+        if hash[:zeros_needed] == (zeros_needed) * "0":
+            print(f"Match found with nonce={nonce} - ", sha.hexdigest())
+            return nonce
+        else:
+            # print(f'[---] Nonce: {nonce} == {hash}')
+            nonce += 1
+            continue
 
-# print(challenge.block)
-print(json.dumps(challenge.block.data, separators=(",", ":")))
+
+def submit_solution(nonce_solution):
+    print('Submitting solution..')
+    r = requests.post(apit_solution_endpoint, json={'nonce': nonce_solution})
+    print(f'Solution submitted. Response: {r.text}')
+def main():
+    challenge_data = get_challenge_data()
+    challenge = Challenge.parse_obj(challenge_data)
+    nonce_solution = brute_force(challenge)
+    submit_solution(nonce_solution)
+
+main()
