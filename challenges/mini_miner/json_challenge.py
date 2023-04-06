7fe5c630cc3db1acd92d692dae96fb34be485801
Sean Mackert <smackert@users.noreply.github.com>
2023-04-06 12:27:01 +0400

    mini_miner: fix: sort block keys, not data

--- challenges/mini_miner/json_challenge.py
+++ challenges/mini_miner/json_challenge.py
@@ -1,12 +1,10 @@
 from typing import List, Tuple, Optional
 from pydantic import BaseModel
 
+
 class Block(BaseModel):
     nonce: Optional[int]
     data: List[Tuple[str, int]]
-
-    def __post_init__(self):
-        self.data = sorted(self.data, key=lambda x: x[0])
 
 
 class Challenge(BaseModel):
